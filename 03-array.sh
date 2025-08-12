a3206c314b6b861a075ef87256b251c479756dd8
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2025-08-12 11:55:46 +0530

    updating arrys

---
+++ 03-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DevOps tool=("Linux" "Git" "aws" "terraform" "CICD" "Jenkins" "Docker" "Kubernetes")
+
+echo "first value: ${DevOps tool[0]}"
+
+echo "second value: ${DevOps tool[1]}"
+
+echo "third value: ${DevOps tool[2]}"
+
+echo "All value: ${DevOps tool[@]}"
