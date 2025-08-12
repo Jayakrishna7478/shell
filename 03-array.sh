40b035cb6f945f79fd8375f33be7ddacf989f957
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2025-08-12 11:59:15 +0530

    Updating arrys values

--- 03-array.sh
+++ 03-array.sh
@@ -2,10 +2,10 @@
 
 DevOpstool=("Linux" "Git" "aws" "terraform" "CICD" "Jenkins" "Docker" "Kubernetes")
 
-echo "first value: ${DevOps tool[0]}"
+echo "first value: ${DevOpstool[0]}"
 
-echo "second value: ${DevOps tool[1]}"
+echo "second value: ${DevOpstool[1]}"
 
-echo "third value: ${DevOps tool[2]}"
+echo "third value: ${DevOpstool[2]}"
 
-echo "All value: ${DevOps tool[@]}"
+echo "All value: ${DevOpstool[@]}"
